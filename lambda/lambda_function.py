63d6229e747b8275ff67964168079574f8613ed9
Mark Christenson <mark.evan.christenson@gmail.com>
2020-08-18 17:24:32 -0400

    feat: implement lambda function to pull aotd data from spotify
    - download aotd playlist data from spotify
    - create album models for first 10 albums
    - save album models to json file
    - upload json file to s3

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,3 +1,4 @@
+import json
 import os
 import sys
 import uuid
@@ -5,6 +6,7 @@ from urllib.parse import unquote_plus
 
 import boto3
 
+from album import Album
 from spotify_client import *
 
 s3_client = boto3.client('s3')
@@ -12,14 +14,24 @@ s3_client = boto3.client('s3')
 def lambda_handler(event, context):
   for record in event['Records']:
     print(record)
-    bucket = record['bucket']
+    config_bucket = record['config_bucket']
     key = unquote_plus(record['key'])
+    storage_bucket = record['storage_bucket']
+    target_file = record['target_file']
+
     tmpkey = key.replace('/', '')
     download_path = '/tmp/{}{}'.format(uuid.uuid4(), tmpkey)
-    s3_client.download_file(bucket, key, download_path)
+    s3_client.download_file(config_bucket, key, download_path)
 
     client_id, secret = parse_credentials(download_path)
-    get_spotify_data(client_id, secret)
+    album_data = get_spotify_data(client_id, secret)
+
+    upload_path = '/tmp/{}{}'.format(uuid.uuid4(), target_file)
+
+    with open(upload_path, 'w') as outfile:
+      json.dump([album.__dict__ for album in album_data], outfile)
+
+    s3_client.upload_file(upload_path, storage_bucket, target_file)
 
 def parse_credentials(download_path):
   with open(download_path) as f:
@@ -28,6 +40,32 @@ def parse_credentials(download_path):
 
 def get_spotify_data(client_id, secret):
   spotify = SpotifyAPI(client_id, secret)
+
   response = spotify.search("AotD 2020", search_type="playlist")
   playlist_id = response['playlists']['items'][0]['id']
-  print(playlist_id)
+  data = spotify.get_playlist(playlist_id)
+
+  return parse_album_data(data)
+
+def parse_album_data(data):
+  tracks = data['tracks']['items']
+
+  albums = []
+  for track in tracks:
+    album_details = track['track']['album']
+    album_name = album_details['name']
+
+    if album_name in [album.name for album in albums]:
+      continue
+    else:
+      name = album_name
+      artist = album_details['artists'][0]['name']
+      url = album_details['href']
+      image = album_details['images'][0]['url']
+      release_date = album_details['release_date']
+      albums.append(Album(name, artist, url, image, release_date))
+
+      if len(albums) >= 10:
+        break
+
+  return albums
