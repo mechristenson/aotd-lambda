9660ef5f7cfde6b487be8f7241acf5da6dfd5c80
Mark Christenson <mark.evan.christenson@gmail.com>
2020-08-18 17:23:33 -0400

    feat: update create-payload script to include album model class

--- lambda/create-payload.sh
+++ lambda/create-payload.sh
@@ -5,3 +5,4 @@ zip -r9 ${curr}/lambda_function_payload.zip .
 cd ${curr}
 zip -g lambda_function_payload.zip lambda_function.py
 zip -g lambda_function_payload.zip spotify_client.py
+zip -g lambda_function_payload.zip album.py
