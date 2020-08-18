d57dfd6e1c6f85982bad79a7a5e307453dcd3025
Mark Christenson <mark.evan.christenson@gmail.com>
2020-08-18 16:17:12 -0400

    feat: update create-payload script to be machine-independent for unix

--- lambda/create-payload.sh
+++ lambda/create-payload.sh
@@ -1,6 +1,7 @@
 rm lambda_function_payload.zip
+curr=$(pwd)
 cd aotd-lambda/lib/python3.8/site-packages/
-zip -r9 ~/Developer/personal/aotd-lambda/lambda/lambda_function_payload.zip .
-cd ~/Developer/personal/aotd-lambda/lambda/
+zip -r9 ${curr}/lambda_function_payload.zip .
+cd ${curr}
 zip -g lambda_function_payload.zip lambda_function.py
 zip -g lambda_function_payload.zip spotify_client.py
