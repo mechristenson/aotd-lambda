b83c85e5c5dcb5cbcea40ef1a9d80f8fbbb16513
Mark Christenson <mark.evan.christenson@gmail.com>
2020-08-18 17:23:07 -0400

    feat: implement album class to represent album data entity

---
+++ lambda/album.py
@@ -0,0 +1,7 @@
+class Album:
+    def __init__(self, name, artist, url, image, release_date):
+        self.name = name
+        self.artist = artist
+        self.url = url
+        self.image = image
+        self.release_date = release_date
